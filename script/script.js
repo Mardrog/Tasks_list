91aeeb5bf051861c479ed7ab2bbffb8903d4b1ab
Pawel Wojnowiak <pawel.wojnowiak@gmail.com>
2021-05-16 19:55:54 +0200

    added emoji buttons to tasks, remove tasks option

--- script/script.js
+++ script/script.js
@@ -6,12 +6,18 @@
         for (const task of tasks) {
             htmlString += `
                 <li>
-                    ${task.content}
+                   <button class = "container__checkTask js-checkTask">&#x2714;</button> ${task.content} <button class = "container__deleteTask js-deleteTask">&#x1F5D1;</delete>
                 </li >
         `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+        const deleteTasks = document.querySelectorAll(".js-deleteTask");
+        deleteTasks.forEach((deleteTask, index) => {
+            deleteTask.addEventListener("click", () => {
+                removeTask(index);
+            });
+        })
     };
     const addNewTask = (newTaskContent) => {
         tasks.push({
@@ -20,6 +26,10 @@
         });
         render();
     };
+    const removeTask = (index) => {
+        tasks.splice(index, 1);
+        render();
+    }
     const onFormSubmit = (event) => {
         event.preventDefault();
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
